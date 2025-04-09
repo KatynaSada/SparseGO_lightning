76f099777c63e19a61f18779b48a9ca9d158a277
Katyna Sada Del Real <ksada@tecnun.es>
2025-04-09 11:28:01 +0200

    new cluster

--- cluster/train_cv.sh
+++ cluster/train_cv.sh
@@ -1,41 +1,40 @@
 # #!/bin/bash
 # nvidia-smi
 # Activate your Python environment
-# source activate /scratch/ksada/envs/SparseGOnew # CHANGE THIS - your environment
-source activate  /Users/katyna/envs/SparseGOnew # CHANGE THIS - your environment
+source activate /scratch/ksada/envs/SparseGOnew # CHANGE THIS - your environment
+# source activate  /Users/katyna/envs/SparseGOnew # CHANGE THIS - your environment
 
 # Login to W&B
 wandb login b1f6d1cea53bb6557df3c1c0c0530b53cadeed3d # CHANGE THIS - your W&B account
 
 # Set OMP_NUM_THREADS to the value of SLURM_CPUS_PER_TASK
-# export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 # Run the Python script with the required arguments
-folder_name="PDCs_expression_LELO" 
-entity="miramon_team"
-project="Hyper_PDCs_CV_LELO_lightning_2" # CHANGE THIS - name you want to give to your W&B project
-tags=selfieslatent184chembl
-foldername="PDCs_expression_LELO" 
+folder_name="PDCs_multiomics_LECO" 
+entity="miramon_team" 
+project="Hyper_PDCs_CV_lightning_final" # CHANGE THIS - name you want to give to your W&B project
+tags=morgan_fingerprint,drug-blind #  selfieslatent184 morgan_fingerprint drug-blind cancer-blind
 
 input_folder="../data/"$folder_name"/"
 output_folder="../results/"$folder_name"/" # CHANGE THIS - folder to store results
 mkdir $output_folder
 
-epochs=200
+epochs=250
 
-num_workers=0
+num_workers=4
 precision="32-true"
 strategy="auto"
 
 min_delta=0.001
-patience=35 
+patience=150 
 early_stopping_metric="validation_corr_spearman"
 
 # Define file paths for datasets, ontology, and other resources
-input_type="expression" # CHANGE 
-gene2id_ont_file="gene2ind.txt" # CHANGE gene2ind gene2ind_ont
-genotype_file="cell2expression.txt" # CHANGE  cell2multiomics
-fingerprint_file="drug2selfieslatent184_new.txt" # CHANGE drug2selfieslatent184 drug2fingerprint
+input_type="multiomics" # CHANGE multiomics mutations expression 
+gene2id_ont_file="gene2ind_ont.txt" # CHANGE gene2ind gene2ind_ont
+genotype_file="cell2multiomics.txt" # CHANGE cell2expression cell2multiomics cell2mutations
+fingerprint_file="drug2fingerprint.txt" # CHANGE drug2selfieslatent184 drug2fingerprint drug2selfieslatent184_new drug2selfieslatent184_largermod
 
 train_file="sparseGO_train.txt"
 validation_file="sparseGO_val.txt"
@@ -49,35 +48,53 @@ gene2id_multiomics_file="gene2ind_multiomics.txt"
 
 # Make output folder for each fold
 samples_folders=samples1,samples2,samples3,samples4,samples5
-for sample_folder in "samples1" "samples2" "samples3" "samples4" "samples5" 
+for sample_folder in "samples1" "samples2" "samples3" "samples4" "samples5" "allsamples"
 do
   mkdir $output_folder$sample_folder
 done
 
+
+#     drug_neurons=$(shuf -e "100,50,6" "80,40" "100," "60,40" "20," "30," "200,100,50" "," -n 1)
+# en pdcs con 4 GPUs no se puede un batch size de mas de 550 aprox
 # Loop to execute the process 200 times
-for i in {1..2}; do
+for i in {1..1}; do
     echo "Running iteration $i..."
     job_type=$(date +%s | md5sum | cut -c 1-8)  # Generates an 8-character random ID based on the current timestamp
     echo "Generated Job ID: $job_type"
 
     # Randomly select hyperparameters
-    batch_size=$(shuf -e 25 52 100 200 300 400 500 1000 -n 1)
-    learning_rate=$(shuf -e 0.0003 0.1 0.01 0.3 0.001 0.2 0.0001 0.05 -n 1)
+    # batch_size=$(shuf -e 2000 5000 10000 15000 30000 -n 1)
+    # learning_rate=$(shuf -e 0.0001 0.0003 0.001 0.003 0.01 0.03 0.1 0.2 0.3 -n 1)
+    # optimizer_type="sgd"  # Static value in this config
+    # decay_rate=$(shuf -e 0 0.003 0.01 0.03 0.1 -n 1)
+    # loss_type="MSELoss"  # Static value
+    # momentum=$(shuf -e 0.85 0.88 0.9 0.93 0.95 0.96 -n 1)
+    # num_neurons_per_GO=$(shuf -e 4 5 6 7 -n 1)
+    # num_neurons_final_GO=$(shuf -e 6 12 24 -n 1)
+    # drug_neurons=$(shuf -e "100,50,6" "80,40" "100,30,15" "60,40" "100,50,100" "30," "200,100,50" -n 1)
+    # num_neurons_final=$(shuf -e 12 18 16 20 -n 1)
+    # p_drop_genes=$(shuf -e 0 0.05 0.1 0.15 -n 1)
+    # p_drop_terms=$(shuf -e 0 0.05 0.1 0.15 -n 1)
+    # p_drop_drugs=$(shuf -e 0 0.05 0.1 0.15 -n 1)
+    # p_drop_final=$(shuf -e 0 0.05 0.1 0.15 -n 1)
+    
+    batch_size=50
+    learning_rate=0.2
     optimizer_type="sgd"  # Static value in this config
-    decay_rate=$(shuf -e 0 0.01 0.0001 0.002 0.003 0.001 -n 1)
+    decay_rate=0.02
     loss_type="MSELoss"  # Static value
-    momentum=$(shuf -e 0.75 0.8 0.85 0.88 0.9 0.95 -n 1)
-    num_neurons_per_GO=$(shuf -e 5 6 -n 1)
-    num_neurons_final_GO=$(shuf -e 6 12 24 -n 1)
-    drug_neurons=$(shuf -e "," -n 1) # drug_neurons=$(shuf -e "80,40" "100," "60,40" "20," "30," "50," -n 1)
-    num_neurons_final=$(shuf -e 12 18 16 20 -n 1)
-    p_drop_genes=$(shuf -e 0 0.05 0.1 0.15 -n 1)
-    p_drop_terms=$(shuf -e 0 0.05 0.1 0.15 -n 1)
-    p_drop_drugs=$(shuf -e 0 0.05 0.1 0.15 -n 1)
-    p_drop_final=$(shuf -e 0 0.05 0.1 0.15 -n 1)
+    momentum=0.85
+    num_neurons_per_GO=6
+    num_neurons_final_GO=6
+    drug_neurons="200,50"
+    num_neurons_final=16
+    p_drop_genes=0.15
+    p_drop_terms=0
+    p_drop_drugs=0.1
+    p_drop_final=0.1
 
     # Create a hyperparameter file
-    hyperparams_file=$output_folder"/hyperparams.txt"
+    hyperparams_file=$output_folder"hyperparams.txt"
 
     # Write the selected hyperparameters to the file
     {
@@ -100,17 +117,15 @@ for i in {1..2}; do
     # Print the selected hyperparameters (for logging)
     echo "Selected Hyperparameters written to $hyperparams_file"
     
-    for sample_folder in "samples1" "samples2" "samples3" "samples4" "samples5" 
+    for sample_folder in "samples1" "samples2" "samples3" "samples4" "samples5" #"samples1" "samples2" "samples3" "samples4" "samples5" 
     do
         # Run the training script with torchrun and the specified hyperparameters $SLURM_NTASKS
-            # --nproc_per_node=$SLURM_NTASKS \
-            # --nnodes=$SLURM_JOB_NUM_NODES \
-            # --node_rank=$SLURM_NODEID \
-            # --master_addr=$(hostname) \
-            # --master_port=$(shuf -i 20000-30000 -n 1) \
         torchrun \
-            --nproc_per_node=1 \
-            --nnodes=1 \
+            --nproc_per_node=$SLURM_NTASKS \
+            --nnodes=$SLURM_JOB_NUM_NODES \
+            --node_rank=$SLURM_NODEID \
+            --master_addr=$(hostname) \
+            --master_port=$(shuf -i 20000-30000 -n 1) \
             ../code/train_cv.py \
             -project $project \
             -entity $entity \
@@ -156,13 +171,13 @@ for i in {1..2}; do
     -predictions_name "d_test_predictions.txt" \
     -labels_name $test_file \
     -genomics_name $genotype_file \
-    -gene2id $gene2id_ont_file \
+    -gene2id $gene2id_multiomics_file \
     -drug2id $drug2id_file \
     -cell2id $cell2id_file \
     -druginput_name $fingerprint_file \
     -drugs_names $drugs_names_file \
+    -input_type $input_type \
     -cuda 0
-    > "$output_folder/metrics_$i.log"
 
     # CAREFUL -gene2id $gene2id_multiomics_file gene2id_ont_file
     python -u ../code/per_drug_correlation.py \
@@ -178,13 +193,13 @@ for i in {1..2}; do
     -predictions_name "s_test_predictions.txt" \
     -labels_name $test_file \
     -genomics_name $genotype_file \
-    -gene2id $gene2id_ont_file \
+    -gene2id $gene2id_multiomics_file \
     -drug2id $drug2id_file \
     -cell2id $cell2id_file \
     -druginput_name $fingerprint_file \
     -drugs_names $drugs_names_file \
+    -input_type $input_type \
     -cuda 0
-    > "$output_folder/metrics2_$i.log"
 
-    echo "Iteration $i completed."
+    # echo "Iteration $i completed."
 done
